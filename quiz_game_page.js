936046a1ce6fb8602f4eeeac03a9a814bc1535ca
aisha100708 <71584539+aisha100708@users.noreply.github.com>
2021-01-25 16:49:41 +0530

    Add files via upload

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -1,3 +1,13 @@
+var player1_score = 0;
+var player2_score = 0;
+var player1_name = localStorage.getItem("player1name");
+var player2_name = localStorage.getItem("player2name");
+document.getElementById("player1_name").innerHTML = player1_name + " : ";
+document.getElementById("player2_name").innerHTML = player2_name + " : ";
+document.getElementById("player1_score").innerHTML = player1_score;
+document.getElementById("player2_score").innerHTML = player2_score;
+document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name;
+document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name;
 function send() {
     number1 = document.getElementById("number_1").value;
     number2 = document.getElementById("number_2").value;
@@ -9,4 +19,36 @@ function send() {
     document.getElementById("output").innerHTML = row;
     document.getElementById("number_1").value = "";
     document.getElementById("number_2").value = "";
+}
+var question_turn = "player_1";
+var answer_turn = "player_2";
+function check() {
+    get_answer = document.getElementById("input_check_box").value;
+    if (get_answer == actual_answer) {
+        if (answer_turn == "player_1") {
+            update_player1_score = player1_score + 1;
+            document.getElementById("player1_score").innerHTML = update_player1_score;
+        }
+        else {
+            update_player2_score = player2_score + 1;
+            document.getElementById("player2_score").innerHTML = update_player2_score;
+        }
+    }
+    if (question_turn == "player_1") {
+        question_turn = "player_2";
+        document.getElementById("player_question").innerHTML = "Question Turn - " + player2_name;
+    }
+    else {
+        question_turn = "player_1";
+        document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name;
+    }
+    if (answer_turn == "player_1") {
+        answer_turn = "player_2";
+        document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name;
+    }
+    else {
+        answer_turn = "player_1";
+        document.getElementById("player_answer").innerHTML = "Answer Turn - " + player1_name;
+    }
+    document.getElementById("output").innerHTML = "";
 }
